2bf31d9ad5a32854dd145ce09d1582b922086b23
CodeHedgehog <codehedgehog@outlook.com>
2018-08-02 20:19:08 -0700

    Add DbDataAdapter to fill DataTable

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 Sample .NET Core console app to explore the usage of DbDataAdapter and DataTable from System.Data.Common 
 
 
+## NuGet Packages
+
+```
++ install-package System.Data.SqlClient
++ install-package System.Configuration.ConfigurationManager
+```
+
+
 ## References
 
 * [Executing Raw SQL Queries using EF Core](https://www.learnentityframeworkcore.com/raw-sql)
--- src/Program.cs
+++ src/Program.cs
@@ -1,39 +1,69 @@
-﻿
-namespace ConsoleApp1
+﻿namespace NetCoreDbDataAdapterDataTableSample
 {
 	using System;
+	using System.Data;
 	using System.Data.Common;
+	using System.Data.SqlClient;
+	using static System.Configuration.ConfigurationManager;
 
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-		}
-
-		public static DataTable ExecuteDataTable(SqlConnection conn, CommandType cmdType, string cmdText, SqlParameter[] cmdParms)
-		{
-			DataTable dt = new DataTable();
-			// just doing this cause dr.load fails
-			dt.Columns.Add("CustomerID");
-			dt.Columns.Add("CustomerName");
-			SqlDataReader dr = ExecuteReader(conn, cmdType, cmdText, cmdParms);
-			// as of now dr.Load throws a big nasty exception saying its not supported. wip.
-			// dt.Load(dr);
-			while (dr.Read())
+			using (SqlConnection theSqlConn = new SqlConnection(connectionString: ConnectionStrings["theDatabaseDbContext"].ConnectionString))
 			{
-				dt.Rows.Add(dr[0], dr[1]);
+
+				theSqlConn.Open();
+				DataTable resultDataTable = FillDataTableUsingSqlDataAdapter(
+					conn: theSqlConn, cmdType: CommandType.Text, 
+				  cmdText:	"SELECT [t].[Description] [TermName] FROM [dbo].[tblTerm] [t] ORDER BY [t].[TermCode] DESC", cmdParms: null);
+				foreach (DataRow term in resultDataTable.Rows)
+				{
+					Console.WriteLine(term[0]);
+				}
+				//try
+				//{
+				//	using (SqlCommand theCommand = new SqlCommand("SELECT * FROM SAMPLETABLE", theSqlConn))
+				//	{
+				//		theCommand.ExecuteNonQuery();
+				//	}
+				//}
+				//catch
+				//{
+				//	Console.WriteLine("Something went wrong");
+				//}
+				//finally
+				//{
+				//	if (theSqlConn != null && theSqlConn.State != ConnectionState.Closed) theSqlConn.Close();
+				//}
+				Console.ReadLine();
 			}
-			return dt;
 		}
 
-		public static DataTable ExecuteDataTableSqlDA(SqlConnection conn, CommandType cmdType, string cmdText, SqlParameter[] cmdParms)
+		public static DataTable FillDataTableUsingSqlDataAdapter(SqlConnection conn, CommandType cmdType, string cmdText, SqlParameter[] cmdParms)
 		{
-			System.Data.DataTable dt = new DataTable();
-			System.Data.SqlClient.SqlDataAdapter da = new SqlDataAdapter(cmdText, conn);
-			da.Fill(dt);
-			return dt;
+			DataTable result = new DataTable();
+			SqlDataAdapter theDataAdapter = new SqlDataAdapter(selectCommandText: cmdText, selectConnection: conn);
+			theDataAdapter.Fill(dataTable: result);
+			return result;
 		}
+
+
+		//public static DataTable ExecuteDataTable(SqlConnection conn, CommandType cmdType, string cmdText, SqlParameter[] cmdParms)
+		//{
+		//	DataTable dt = new DataTable();
+		//	// just doing this cause dr.load fails
+		//	dt.Columns.Add("CustomerID");
+		//	dt.Columns.Add("CustomerName");
+		//	DbDataReader dr = ExecuteReader(conn, cmdType, cmdText, cmdParms);
+		//	// dt.Load(dr);
+		//	while (dr.Read())
+		//	{
+		//		dt.Rows.Add(dr[0], dr[1]);
+		//	}
+		//	return dt;
+		//}
+
 
 	}
 }
